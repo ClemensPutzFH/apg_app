3d1174dd30e6fe9234ebb33a7f81de3df32b3837
ClemensPutzFH <putz782@gmail.com>
2023-06-26 18:01:19 +0200

    first work

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:apg_app/benachrichtigungstext.dart';
+import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:http/http.dart' as http;
@@ -22,6 +23,57 @@ bool redTest = false;
 bool notificationTest = true;
 
 late SpitzenStundenObject spitzenStundenData;
+
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  var taskId = task.taskId;
+  print("[BackgroundFetch] Headless event received.");
+  if (taskId == "your_task_id") {
+    print("your_task_id");
+    print("[BackgroundFetch] Headless event received.");
+//TODO: perform tasks like — call api, DB and local notification etc…
+  }
+}
+
+Future<void> initPlatformState() async {
+// Configure BackgroundFetch.
+  var status = await BackgroundFetch.configure(
+      BackgroundFetchConfig(
+        minimumFetchInterval: 15,
+        forceAlarmManager: false,
+        stopOnTerminate: false,
+        startOnBoot: true,
+        enableHeadless: true,
+        requiresBatteryNotLow: false,
+        requiresCharging: false,
+        requiresStorageNotLow: false,
+        requiresDeviceIdle: false,
+        requiredNetworkType: NetworkType.NONE,
+      ),
+      _onBackgroundFetch,
+      _onBackgroundFetchTimeout);
+  print("[BackgroundFetch] configure success: $status");
+// Schedule backgroundfetch for the 1st time it will execute with 1000ms delay.
+// where device must be powered (and delay will be throttled by the OS).
+  BackgroundFetch.scheduleTask(TaskConfig(
+      taskId: "com.dltlabs.task",
+      delay: 1000,
+      periodic: false,
+      stopOnTerminate: false,
+      enableHeadless: true));
+}
+
+void _onBackgroundFetchTimeout(String taskId) {
+  print("[BackgroundFetch] TIMEOUT: $taskId");
+  BackgroundFetch.finish(taskId);
+}
+
+void _onBackgroundFetch(String taskId) async {
+  print("[BackgroundFetch] Event received");
+  if (taskId == "your_task_id") {
+    print("[BackgroundFetch] Event received");
+//TODO: perform your task like : call the API’s, call the DB and local notification.
+  }
+}
 
 void main() async {
   AwesomeNotifications().initialize(
@@ -47,6 +99,8 @@ void main() async {
   await inizializeService();
 
   runApp(const MyApp());
+
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
 }
 
 class MyApp extends StatelessWidget {
