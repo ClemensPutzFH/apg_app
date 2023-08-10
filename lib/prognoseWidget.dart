2cfd818d79a4ceede833ddb1ff43c152c443cf4f
ClemensPutzFH <putz782@gmail.com>
2023-08-10 14:55:52 +0200

    5 days added

--- lib/prognoseWidget.dart
+++ lib/prognoseWidget.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:convert';
 import 'dart:math';
 import 'dart:ui';
@@ -29,353 +30,121 @@ class PrognoseView extends StatelessWidget {
     DateTime lastUtc = DateTime.parse(data.lastUtc);
 
     var test = lastUtc.difference(firstUtc).inHours;
-    print("hallo hallo" + test.toString());
 
-    for (var i = 0; i < lastUtc.difference(firstUtc).inHours; i++) {}
+    print("Day: " + firstUtc.toLocal().weekday.toString());
 
-    return // Generated code for this Column Widget...
-        LayoutBuilder(
-      builder: (BuildContext buildContext, BoxConstraints constraints) {
-        return Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
+    List<Widget> prognoseDays = List<Widget>.empty(growable: true);
+    List<Widget> hourTiles = List<Widget>.empty(growable: true);
+
+    for (var i = 1; i <= lastUtc.difference(firstUtc).inHours; i++) {
+      var iTime = lastUtc.toLocal().add(Duration(hours: i));
+
+      hourTiles.add(getHourTile(1));
+
+      if (i % 24 == 0) {
+        prognoseDays
+            .add(getPrognoseRow(context, List.from(hourTiles), iTime.weekday));
+        hourTiles = List<Widget>.empty(growable: true);
+      }
+    }
+
+    return Column(
+      children: prognoseDays,
+    );
+  }
+}
+
+Widget getPrognoseRow(context, List<Widget> rowTileList, day) {
+  return LayoutBuilder(
+    builder: (BuildContext buildContext, BoxConstraints constraints) {
+      return Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.075,
+              ),
+              Text(
+                'Heute',
+              ),
+              Spacer()
+            ],
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
               children: [
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.075,
                 ),
-                Text(
-                  'Heute',
-                ),
-                Spacer()
-              ],
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.075,
-                  ),
-                  Container(
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 510,
-                          height: 30.0,
-                          decoration: BoxDecoration(),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                ' 0 Uhr',
-                              ),
-                              Text(
-                                ' 4 Uhr',
-                              ),
-                              Text(
-                                ' 8 Uhr',
-                              ),
-                              Text(
-                                '12 Uhr',
-                              ),
-                              Text(
-                                '16 Uhr',
-                              ),
-                              Text(
-                                '20 Uhr',
-                              ),
-                              Text(
-                                '24 Uhr',
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
+                Container(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 510,
+                        height: 30.0,
+                        decoration: BoxDecoration(),
+                        child: Row(
                           mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              ' 0 Uhr',
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              ' 4 Uhr',
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              ' 8 Uhr',
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              '12 Uhr',
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              '16 Uhr',
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              '20 Uhr',
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              width: 20.0,
-                              height: 50.0,
-                              decoration: BoxDecoration(
-                                color: Color(0xFFFF00FF),
-                                shape: BoxShape.rectangle,
-                                border: Border.all(
-                                  color: Color(0xFF00FFFF),
-                                ),
-                              ),
+                            Text(
+                              '24 Uhr',
                             ),
                           ],
                         ),
-                      ],
-                    ),
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: rowTileList,
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.075,
-                  )
-                ],
-              ),
-            )
-          ],
-        );
-      },
-    );
-  }
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.075,
+                )
+              ],
+            ),
+          )
+        ],
+      );
+    },
+  );
+}
+
+Widget getHourTile(status) {
+  return Container(
+    width: 20.0,
+    height: 50.0,
+    decoration: BoxDecoration(
+      color: Color(0xFFFF00FF),
+      shape: BoxShape.rectangle,
+      border: Border.all(
+        color: Color(0xFF00FFFF),
+      ),
+    ),
+  );
 }
